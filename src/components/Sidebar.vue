ca1ed3e804f5b4b8e25c47f6e8b8c8819e7595b7
qq1999531 <116276637+qq1999531@users.noreply.github.com>
2022-12-28 16:44:16 +0800

    Add files via upload

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+    
+    <nav class="sidebar white-theme" :style="{ width: (200 * showSidebarFactor)+'px' }" @mouseenter="toggleSidebar" @mouseleave="toggleSidebar">
+        {{showSidebar}}{{(200 * showSidebarFactor)+'px'}}
+        <div class="clock" :style="{ width: (200 * showSidebarFactor)+'px' }">
+            <Clock1 :showSidebarFactor="showSidebarFactor"/>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import Clock1 from '@/components/clocks/Clock1.vue'
+import { computed, ref } from 'vue'
+const showSidebar = ref(true)
+const showSidebarFactor = computed(() => {return showSidebar.value ? 1 : 0.2})
+function toggleSidebar() {
+    showSidebar.value = !showSidebar.value
+}
+</script>
+
+<style lang="scss">
+.sidebar{
+    height: 100%;
+    left: 0px;
+    padding: 0px;
+    position: fixed;
+    border-right: 1px solid rgb(127, 127, 127);
+    transition: 0.2s;
+    .white-theme{
+        background-color: rgb(255, 255, 255);
+    }
+    .black-theme{
+        background-color: rgb(0, 0, 0);
+    }
+}
+
+.clock{
+    position: fixed;
+    overflow: hidden;
+    transition: 0.2s;
+}
+</style>
---
+++ src/components/clocks/Clock1.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg>
+        <circle :cx="50 * showSidebarFactor" :cy="50 * showSidebarFactor" :r="50 * showSidebarFactor"></circle>
+    </svg>
+    {{50 * showSidebarFactor}}
+</template>
+
+<script setup>
+    const { computed }=require("@vue/runtime-core")
+
+    defineProps({
+        showSidebarFactor : Number
+    })
+</script>
+
+<style lang="scss">
+
+</style>
