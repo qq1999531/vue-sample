28f387fae7120a3dee93056c47a48bf492024224
qq1999531 <116276637+qq1999531@users.noreply.github.com>
2022-12-28 23:28:20 +0800

    Add files via upload

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,9 +2,11 @@
     
     <nav class="sidebar white-theme" :style="{ width: (200 * showSidebarFactor)+'px' }" @mouseenter="toggleSidebar" @mouseleave="toggleSidebar">
         {{showSidebar}}{{(200 * showSidebarFactor)+'px'}}
-        <div class="clock" :style="{ width: (200 * showSidebarFactor)+'px' }">
-            <Clock1 :showSidebarFactor="showSidebarFactor"/>
-        </div>
+        <transition name="fade">
+            <div v-show="showSidebar" class="clock" :style="{ width: (200 * showSidebarFactor)+'px' }">
+                <Clock1 :showSidebarFactor="showSidebarFactor" />
+            </div>
+        </transition>
     </nav>
 </template>
 
@@ -20,23 +22,52 @@ function toggleSidebar() {
 
 <style lang="scss">
 .sidebar{
+    overflow: hidden;
     height: 100%;
     left: 0px;
     padding: 0px;
     position: fixed;
     border-right: 1px solid rgb(127, 127, 127);
     transition: 0.2s;
-    .white-theme{
-        background-color: rgb(255, 255, 255);
-    }
-    .black-theme{
-        background-color: rgb(0, 0, 0);
-    }
 }
-
+.white-theme{
+    background-color: rgb(255, 255, 255);
+}
+.black-theme{
+    background-color: rgb(0, 0, 0);
+}
 .clock{
     position: fixed;
     overflow: hidden;
     transition: 0.2s;
+    justify-content: center;
+    align-items: center;
+    svg circle{
+        fill: transparent;
+        stroke-width: 6;
+        //stroke: var(--clr);
+        stroke: #7f7f7f;
+        stroke-linecap: round;
+        stroke-dasharray: 440;
+        stroke-dashoffset: 440;
+    }
+}
+.fade-enter-from{
+    opacity: 0;
+}
+.fade-enter-to{
+    opacity: 1;
+}
+.fade-enter-active{
+    transition: all 0.2s ease ;
+}
+.fade-leave-from{
+    opacity: 1;
+}
+.fade-leave-to{
+    opacity: 0;
+}
+.fade-leave-active{
+    transition: all 0.2s ease ;
 }
 </style>
--- src/components/clocks/Clock1.vue
+++ src/components/clocks/Clock1.vue
@@ -1,12 +1,27 @@
 <template>
     <svg>
-        <circle :cx="100 * showSidebarFactor" :cy="50 * showSidebarFactor" :r="50 * showSidebarFactor"></circle>
+        <filter id="glow">
+				<feGaussianBlur stdDeviation="2.5" result="coloredBlur"/>
+				<feMerge>
+						<feMergeNode in="coloredBlur"/>
+						<feMergeNode in="SourceGraphic"/>
+				</feMerge>
+		</filter>		
+		
+		<filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
+			<feGaussianBlur in="SourceAlpha" stdDeviation="3" result="shadow"/>
+			<feOffset dx="1" dy="1"/>
+			<feMerge>
+				<feMergeNode/>
+				<feMergeNode in="SourceGraphic"/>
+			</feMerge>
+		</filter>		
+        <circle :cx="100" :cy="50" :r="50" style="--clr:#7f7f7f;"></circle>
     </svg>
-    {{50 * showSidebarFactor}}
 </template>
 
 <script setup>
-    const { computed }=require("@vue/runtime-core")
+    import { computed } from "@vue/runtime-core"
 
     defineProps({
         showSidebarFactor : Number
